068b394322b9daa91028be03de8b8293de4d3f5e
Ben Weier <ben.weier@gmail.com>
2015-02-08 18:55:02 +1000

    Fix module paths

--- battlenet-api.js
+++ battlenet-api.js
@@ -16,26 +16,26 @@ module.exports = function(options) {
   var battlenet = require('./lib/core')(request, extend, options);
 
   return {
-    account: require('./account/user')(battlenet),
+    account: require('./lib/account/user')(battlenet),
     wow: {
-      achievement: require('./wow/achievement')(battlenet),
-      auction: require('./wow/auction')(battlenet),
-      battlePet: require('./wow/battlepet')(battlenet),
-      challenge: require('./wow/challenge')(battlenet),
-      character: require('./wow/character')(battlenet),
-      data: require('./wow/data')(battlenet),
-      guild: require('./wow/guild')(battlenet),
-      item: require('./wow/item')(battlenet),
-      pvp: require('./wow/pvp')(battlenet),
-      quest: require('./wow/quest')(battlenet),
-      realmStatus: require('./wow/realm')(battlenet),
-      recipe: require('./wow/recipe')(battlenet),
-      spell: require('./wow/spell')(battlenet)
+      achievement: require('./lib/wow/achievement')(battlenet),
+      auction: require('./lib/wow/auction')(battlenet),
+      battlePet: require('./lib/wow/battlepet')(battlenet),
+      challenge: require('./lib/wow/challenge')(battlenet),
+      character: require('./lib/wow/character')(battlenet),
+      data: require('./lib/wow/data')(battlenet),
+      guild: require('./lib/wow/guild')(battlenet),
+      item: require('./lib/wow/item')(battlenet),
+      pvp: require('./lib/wow/pvp')(battlenet),
+      quest: require('./lib/wow/quest')(battlenet),
+      realmStatus: require('./lib/wow/realm')(battlenet),
+      recipe: require('./lib/wow/recipe')(battlenet),
+      spell: require('./lib/wow/spell')(battlenet)
     },
     sc2: {
-      data: require('./sc2/data')(battlenet),
-      ladder: require('./sc2/ladder')(battlenet),
-      profile: require('./sc2/profile')(battlenet)
+      data: require('./lib/sc2/data')(battlenet),
+      ladder: require('./lib/sc2/ladder')(battlenet),
+      profile: require('./lib/sc2/profile')(battlenet)
     },
     d3: {
       data: require('./d3/data')(battlenet),
