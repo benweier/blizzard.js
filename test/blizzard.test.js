f890d46794acedcd7f80eba7f4057d3e0d462d49
Ben Weier <ben.weier@gmail.com>
2016-10-12 23:48:36 +1000

    Add tests for get method

--- test/blizzard.test.js
+++ test/blizzard.test.js
@@ -5,11 +5,15 @@ require('dotenv').config({ silent: true });
 
 const path = require('path');
 const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
 const blizzard = require(path.normalize(`${__dirname}/../index.js`)).initialize({
   apikey: process.env.BATTLENET_API_KEY
 });
 
+chai.use(chaiAsPromised);
+
 describe('lib/blizzard.js', function () {
+  this.slow(5000);
 
   context('API interfaces', function () {
     const tests = ['account', 'd3', 'sc2', 'wow'];
@@ -32,6 +36,21 @@ describe('lib/blizzard.js', function () {
       chai.assert.property(filtered, 'three');
       chai.assert.notProperty(filtered, 'four');
       done();
+    });
+  });
+
+  context('.get()', function () {
+
+    it('should eventually be fulfilled', function (done) {
+      const request = blizzard.get('/wow/character/amanthul/charni', { realm: 'amanthul', name: 'charni' });
+
+      return chai.assert.isFulfilled(request);
+    });
+
+    it('should eventually be rejected', function () {
+      const request = blizzard.get('/wow/character/amanthul/lolthisdoesnotexist');
+
+      return chai.assert.isRejected(request);
     });
   });
 
