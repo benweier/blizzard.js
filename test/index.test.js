19c8e83cdb2d799ffe4880e4055a4c06aaee1dfd
Ben Weier <ben.weier@gmail.com>
2016-10-13 10:20:46 +1000

    Use common initialization module

--- test/index.test.js
+++ test/index.test.js
@@ -3,14 +3,14 @@
 
 const path = require('path');
 const chai = require('chai');
-const blizzard = require(path.normalize(`${__dirname}/../index.js`)).initialize();
+const blizzard = require('./initialize');
 const prototype = require(path.normalize(`${__dirname}/../lib/blizzard.js`));
 
 describe('index.js', function () {
 
   context('.initialize()', function () {
-    it('should return an instance of Blizzard.js', function (done) {
-      chai.expect(blizzard).to.be.an.instanceOf(prototype);
+    it('should return an instance of blizzard.js', function (done) {
+      chai.assert.instanceOf(blizzard, prototype, 'blizzard is an instance of blizzard.js');
       done();
     });
   });
