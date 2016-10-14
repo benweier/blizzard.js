173fe3dc1562f09ba0cb839aba09c758b88e63a3
Ben Weier <ben.weier@gmail.com>
2016-10-14 18:15:40 +1000

    Add test for sc2 profile

--- test/sc2.test.js
+++ test/sc2.test.js
@@ -9,6 +9,7 @@ const blizzard = require('./initialize');
 chai.use(chaiAsPromised);
 
 describe('lib/sc2.js', function () {
+  this.timeout(10000);
 
   context('API methods', function () {
     const tests = ['data', 'ladder', 'profile'];
@@ -18,6 +19,14 @@ describe('lib/sc2.js', function () {
         chai.assert.isFunction(blizzard.sc2[test]);
         done();
       });
+    });
+  });
+
+  context('.profile()', function () {
+    it('should eventually return a user profile', function () {
+      const profile = blizzard.sc2.profile('profile', { id: 2137104, name: 'skt' });
+
+      return chai.assert.eventually.deepProperty(profile, 'data.id');
     });
   });
 
