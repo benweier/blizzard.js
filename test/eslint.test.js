58b79ef8c385f2d5d8d17b1adc5fa8b3092e2197
Ben Weier <ben.weier@gmail.com>
2017-07-26 09:40:47 +1000

    Replace mocha-eslint test

--- test/eslint.test.js
+++ test/eslint.test.js
@@ -1,6 +1,26 @@
-/* global describe */
-const lint = require('mocha-eslint');
+const CLIEngine = require('eslint').CLIEngine;
+const chalk = require('chalk');
+const glob = require('glob-all');
+const cli = new CLIEngine({});
 
-describe('Code Standards', function () {
+const lint = function lint (patterns) {
+  describe('eslint', () => {
+
+    glob.sync(patterns).forEach(file => {
+      test('should have no errors in ' + file, function () {
+        const report = cli.executeOnFiles([file]);
+
+        if (report.errorCount) {
+          throw new Error(
+            chalk.red('Code does not pass linting rules')
+          );
+        }
+      });
+    });
+
+  });
+};
+
+describe('Code Standards', () => {
   lint(['index.js', 'lib/**/*.js', 'test/**/*.js']);
 });
