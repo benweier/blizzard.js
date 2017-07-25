fd7a013124a6aa29ba06c361dfe454a591956cc3
Ben Weier <ben.weier@gmail.com>
2017-07-25 13:58:49 +1000

    Update endpoint tests

--- test/endpoints.test.js
+++ test/endpoints.test.js
@@ -10,22 +10,22 @@ describe('lib/endpoints.js', () => {
 
   describe('#getEndpoint()', () => {
 
-    test('with no parameters should return the default endpoint', () => {
+    test('should return the default endpoint', () => {
       const endpoint = endpoints.getEndpoint();
       expect(endpoint).toMatchSnapshot();
     });
 
-    test('with invalid parameters should return the default endpoint', () => {
+    test('should return the US endpoint', () => {
       const endpoint = endpoints.getEndpoint('foo');
       expect(endpoint).toMatchSnapshot();
     });
 
-    test('with valid parameters should return the SEA endpoint', () => {
+    test('should return the SEA endpoint', () => {
       const endpoint = endpoints.getEndpoint('sea');
       expect(endpoint).toMatchSnapshot();
     });
 
-    test('with valid parameters should return the EU es_ES endpoint', () => {
+    test('should return the EU es_ES endpoint', () => {
       const endpoint = endpoints.getEndpoint('eu', 'es_ES');
       expect(endpoint).toMatchSnapshot();
     });
