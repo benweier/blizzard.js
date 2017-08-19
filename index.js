24e79641b2e2def2592ba986b6a1566c198b663d
Ben Weier <ben.weier@gmail.com>
2017-08-19 13:56:36 +1000

    Update function arg descriptions

--- index.js
+++ index.js
@@ -11,9 +11,9 @@
 /**
  * @typedef {Object} Blizzard
  * @prop    {Object} account     Account API methods
- * @prop    {Object} d3          D3 API methods
- * @prop    {Object} sc2         SC2 API methods
- * @prop    {Object} wow         WoW API methods
+ * @prop    {Object} d3          Diablo 3 API methods
+ * @prop    {Object} sc2         Starcraft 2 API methods
+ * @prop    {Object} wow         World of Warcraft API methods
  * @prop    {Function} battletag Get a URI safe battletag
  * @prop    {Function} params    Filter an objects keys from an array of keys
  * @prop    {Function} get       Perform a single request to the Blizzard API
@@ -25,10 +25,10 @@ const Blizzard = require('./lib/blizzard');
  * Initialize the Blizzard.js instance.
  *
  * @param  {Object} args                Blizzard.js configuration options
- * @param  {String} args.apikey         Your private Blizzard API key
- * @param  {String} [args.access_token] A default user access token
- * @param  {String} [args.origin]       The default API region
- * @param  {String} [args.locale]       The default API locale
+ * @param  {String} args.apikey         The default Blizzard API key
+ * @param  {String} [args.access_token] The default user access token
+ * @param  {String} [args.origin=US]    The default API region
+ * @param  {String} [args.locale=en_US] The default API locale
  * @param  {Object} axios               An [axios](https://github.com/mzabriskie/axios) compatible instance configuration
  * @return {Blizzard}                   An instance of Blizzard.js
  */
