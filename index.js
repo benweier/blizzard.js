bc088c4eaea35e670b8eac09b7437e61c2687975
Ben Weier <ben.weier@gmail.com>
2018-12-26 19:05:50 +1000

    Rename `id` to `key` #75

--- index.js
+++ index.js
@@ -15,7 +15,6 @@
  * @prop    {Object} sc2         Starcraft 2 API methods
  * @prop    {Object} wow         World of Warcraft API methods
  * @prop    {Function} battletag Get a URI safe battletag
- * @prop    {Function} params    Filter an objects keys from an array of keys
  * @prop    {Function} get       Perform a single request to the Blizzard API
  * @prop    {Function} all       Perform multiple requests to the Blizzard API
  */
@@ -27,14 +26,14 @@ const Blizzard = require('./lib/blizzard');
  * @param  {Object} args          Blizzard.js configuration options
  * @param  {String} [args.origin] The default API region
  * @param  {String} [args.locale] The default API locale
- * @param  {String} [args.id]     The default API client ID
+ * @param  {String} [args.key]    The default API client ID
  * @param  {String} [args.secret] The default API client secret
  * @param  {String} [args.token]  The default API access token
- * @param  {Object} axios         An [axios](https://github.com/mzabriskie/axios) compatible instance configuration
+ * @param  {Object} instance      An [axios](https://github.com/mzabriskie/axios) compatible instance configuration
  * @return {Blizzard}             An instance of Blizzard.js
  */
-const initialize = function initialize ({ origin, locale, id, secret, token }, axios = {}) {
-  return new Blizzard({ origin, locale, id, secret, token }, axios);
+const initialize = function initialize({ origin, locale, key, secret, token }, instance = {}) {
+  return new Blizzard({ origin, locale, key, secret, token }, instance);
 };
 
 exports.initialize = initialize;
