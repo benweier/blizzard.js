c1abf219dadfbee430ef0e174e1648f4ceb85622
Ben Weier <ben.weier@gmail.com>
2018-12-26 22:11:40 +1000

    Update Game Data API unit tests #75

--- test/data.test.js
+++ test/data.test.js
@@ -1,30 +1,35 @@
 const blizzard = require('./initialize');
 
 describe('lib/data.js', () => {
-
   beforeEach(() => {
     blizzard.axios.get.mockClear();
   });
 
   test('should have the correct API methods', () => {
-    expect(blizzard.data).toEqual(expect.objectContaining({
-      credentials: expect.any(Function),
-      validate: expect.any(Function),
-      connectedRealm: expect.any(Function),
-      mythicLeaderboard: expect.any(Function),
-      realm: expect.any(Function),
-      region: expect.any(Function),
-      token: expect.any(Function),
-    }));
+    expect(blizzard.data).toEqual(
+      expect.objectContaining({
+        credentials: expect.any(Function),
+        validate: expect.any(Function),
+        connectedRealm: expect.any(Function),
+        keystoneAffix: expect.any(Function),
+        mythicRaidLeaderboard: expect.any(Function),
+        mythicDungeonKeystone: expect.any(Function),
+        mythicKeystoneLeaderboard: expect.any(Function),
+        playableClass: expect.any(Function),
+        pvpTalentSlots: expect.any(Function),
+        playableSpecialization: expect.any(Function),
+        powerType: expect.any(Function),
+        playableRace: expect.any(Function),
+        realm: expect.any(Function),
+        region: expect.any(Function),
+        token: expect.any(Function),
+      }),
+    );
   });
 
   describe('#credentials()', () => {
-    test('should request an application access token', () => {
-      blizzard.data.credentials({
-        origin: 'us',
-        id: 'id',
-        secret: 'secret',
-      });
+    test('should request an application access token with default parameters', () => {
+      blizzard.data.credentials();
 
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
       expect(blizzard.axios.get).toHaveBeenCalledWith(
@@ -32,10 +37,34 @@ describe('lib/data.js', () => {
         expect.objectContaining({
           params: {
             grant_type: 'client_credentials',
-            client_id: 'id',
-            client_secret: 'secret',
           },
-        })
+          auth: {
+            username: 'key',
+            password: 'secret',
+          },
+        }),
+      );
+    });
+
+    test('should request an application access token with override parameters', () => {
+      blizzard.data.credentials({
+        origin: 'eu',
+        key: 'yek',
+        secret: 'terces',
+      });
+
+      expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
+      expect(blizzard.axios.get).toHaveBeenCalledWith(
+        'https://eu.battle.net/oauth/token',
+        expect.objectContaining({
+          params: {
+            grant_type: 'client_credentials',
+          },
+          auth: {
+            username: 'yek',
+            password: 'terces',
+          },
+        }),
       );
     });
   });
@@ -49,7 +78,7 @@ describe('lib/data.js', () => {
         'https://us.battle.net/oauth/check_token',
         expect.objectContaining({
           params: { token: 'test' },
-        })
+        }),
       );
     });
   });
@@ -60,8 +89,8 @@ describe('lib/data.js', () => {
 
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
       expect(blizzard.axios.get).toHaveBeenCalledWith(
-        'https://us.api.blizzard.com/data/wow/connected-realm/',
-        expect.any(Object)
+        'https://us.api.blizzard.com/data/wow/connected-realm/index',
+        expect.any(Object),
       );
     });
 
@@ -71,41 +100,29 @@ describe('lib/data.js', () => {
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
       expect(blizzard.axios.get).toHaveBeenCalledWith(
         'https://us.api.blizzard.com/data/wow/connected-realm/11',
-        expect.any(Object)
+        expect.any(Object),
       );
     });
   });
 
-  describe('#mythicLeaderboard()', () => {
+  describe('#mythicKeystoneLeaderboard()', () => {
     test('should request the mythic leaderboard index', () => {
-      blizzard.data.mythicLeaderboard({ realm: 11, origin: 'us' });
+      blizzard.data.mythicKeystoneLeaderboard({ realmID: 11, origin: 'us' });
 
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
       expect(blizzard.axios.get).toHaveBeenCalledWith(
-        'https://us.api.blizzard.com/data/wow/connected-realm/11/mythic-leaderboard/',
-        expect.any(Object)
+        'https://us.api.blizzard.com/data/wow/connected-realm/11/mythic-leaderboard/index',
+        expect.any(Object),
       );
     });
 
     test('should request a mythic leaderboard period', () => {
-      blizzard.data.mythicLeaderboard({ realm: 11, dungeon: 200, period: 606, origin: 'us' });
+      blizzard.data.mythicKeystoneLeaderboard({ realmID: 11, dungeonID: 200, periodID: 606, origin: 'us' });
 
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
       expect(blizzard.axios.get).toHaveBeenCalledWith(
         'https://us.api.blizzard.com/data/wow/connected-realm/11/mythic-leaderboard/200/period/606',
-        expect.any(Object)
-      );
-    });
-  });
-
-  describe('#mythicChallengeMode()', () => {
-    test('should request the mythic challenge mode index', () => {
-      blizzard.data.mythicChallengeMode({ realm: 11, origin: 'us' });
-
-      expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
-      expect(blizzard.axios.get).toHaveBeenCalledWith(
-        'https://us.api.blizzard.com/data/wow/mythic-challenge-mode/',
-        expect.any(Object)
+        expect.any(Object),
       );
     });
   });
@@ -116,18 +133,18 @@ describe('lib/data.js', () => {
 
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
       expect(blizzard.axios.get).toHaveBeenCalledWith(
-        'https://us.api.blizzard.com/data/wow/realm/',
-        expect.any(Object)
+        'https://us.api.blizzard.com/data/wow/realm/index',
+        expect.any(Object),
       );
     });
 
     test('should request a single realm', () => {
-      blizzard.data.realm({ realm: 11, origin: 'us' });
+      blizzard.data.realm({ realm: 'tichondrius', origin: 'us' });
 
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
       expect(blizzard.axios.get).toHaveBeenCalledWith(
-        'https://us.api.blizzard.com/data/wow/realm/11',
-        expect.any(Object)
+        'https://us.api.blizzard.com/data/wow/realm/tichondrius',
+        expect.any(Object),
       );
     });
   });
@@ -138,18 +155,18 @@ describe('lib/data.js', () => {
 
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
       expect(blizzard.axios.get).toHaveBeenCalledWith(
-        'https://us.api.blizzard.com/data/wow/region/',
-        expect.any(Object)
+        'https://us.api.blizzard.com/data/wow/region/index',
+        expect.any(Object),
       );
     });
 
     test('should request a single region', () => {
-      blizzard.data.region({ region: 1, origin: 'us' });
+      blizzard.data.region({ regionID: 1, origin: 'us' });
 
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
       expect(blizzard.axios.get).toHaveBeenCalledWith(
         'https://us.api.blizzard.com/data/wow/region/1',
-        expect.any(Object)
+        expect.any(Object),
       );
     });
   });
@@ -161,9 +178,8 @@ describe('lib/data.js', () => {
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
       expect(blizzard.axios.get).toHaveBeenCalledWith(
         'https://us.api.blizzard.com/data/wow/token/',
-        expect.any(Object)
+        expect.any(Object),
       );
     });
   });
-
 });
