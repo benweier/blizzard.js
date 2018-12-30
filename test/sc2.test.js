bab499010f14018ab92e7ed859731ee368d24238
Ben Weier <ben.weier@gmail.com>
2018-12-30 16:21:51 +1000

    Update unit tests for new SC2 API methods #75

--- test/sc2.test.js
+++ test/sc2.test.js
@@ -17,42 +17,57 @@ describe('lib/sc2.js', () => {
   test('should have the correct API methods', () => {
     expect(blizzard.sc2).toEqual(
       expect.objectContaining({
-        data: expect.any(Function),
         ladder: expect.any(Function),
+        static: expect.any(Function),
+        metadata: expect.any(Function),
         profile: expect.any(Function),
+        profileLadder: expect.any(Function),
+        player: expect.any(Function),
       }),
     );
   });
 
-  describe('#data()', () => {
-    test('should request achievements data', () => {
-      blizzard.sc2.data('achievements');
+  describe('#static()', () => {
+    test('should request static data by region ID', () => {
+      blizzard.sc2.static({ region: 1 });
 
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
       expect(blizzard.axios.get).toHaveBeenCalledWith(
-        'https://us.api.blizzard.com/sc2/data/achievements',
+        'https://us.api.blizzard.com/sc2/static/profile/1',
         expect.objectContaining(args),
       );
     });
+  });
 
-    test('should request rewards data', () => {
-      blizzard.sc2.data('rewards');
+  describe('#metadata()', () => {
+    test('should request metadata by region and realm and profile', () => {
+      blizzard.sc2.metadata({ profile: 2137104, realm: 1, region: 1 });
 
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
       expect(blizzard.axios.get).toHaveBeenCalledWith(
-        'https://us.api.blizzard.com/sc2/data/rewards',
+        'https://us.api.blizzard.com/sc2/metadata/profile/1/1/2137104',
         expect.objectContaining(args),
       );
     });
   });
 
   describe('#ladder()', () => {
-    test('should request a ladder by ID', () => {
-      blizzard.sc2.ladder({ id: 194163 });
+    test('should request a season ladder by ID', () => {
+      blizzard.sc2.ladder({ region: 1 });
 
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
       expect(blizzard.axios.get).toHaveBeenCalledWith(
-        'https://us.api.blizzard.com/sc2/ladder/194163',
+        'https://us.api.blizzard.com/sc2/ladder/season/1',
+        expect.objectContaining(args),
+      );
+    });
+
+    test('should request a grandmaster ladder by ID', () => {
+      blizzard.sc2.ladder({ ladder: 'grandmaster', region: 1 });
+
+      expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
+      expect(blizzard.axios.get).toHaveBeenCalledWith(
+        'https://us.api.blizzard.com/sc2/ladder/grandmaster/1',
         expect.objectContaining(args),
       );
     });
@@ -60,31 +75,45 @@ describe('lib/sc2.js', () => {
 
   describe('#profile()', () => {
     test('should request a player profile', () => {
-      blizzard.sc2.profile('profile', { id: 2137104, name: 'skt' });
+      blizzard.sc2.profile({ profile: 2137104, realm: 1, region: 1 });
 
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
       expect(blizzard.axios.get).toHaveBeenCalledWith(
-        'https://us.api.blizzard.com/sc2/profile/2137104/1/skt/',
+        `https://us.api.blizzard.com/sc2/profile/1/1/2137104`,
+        expect.objectContaining(args),
+      );
+    });
+  });
+
+  describe('#profileLadder()', () => {
+    test('should request a profile ladder summary', () => {
+      blizzard.sc2.profileLadder({ profile: 194163, realm: 1, region: 1 });
+
+      expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
+      expect(blizzard.axios.get).toHaveBeenCalledWith(
+        'https://us.api.blizzard.com/sc2/profile/1/1/194163/ladder/summary',
         expect.objectContaining(args),
       );
     });
 
-    test('should request a player ladder placements', () => {
-      blizzard.sc2.profile('ladders', { id: 2137104, name: 'skt' });
+    test('should request a profile ladder by ID', () => {
+      blizzard.sc2.profileLadder({ profile: 194163, realm: 1, region: 1, ladder: 1 });
 
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
       expect(blizzard.axios.get).toHaveBeenCalledWith(
-        'https://us.api.blizzard.com/sc2/profile/2137104/1/skt/ladders',
+        'https://us.api.blizzard.com/sc2/profile/1/1/194163/ladder/1',
         expect.objectContaining(args),
       );
     });
+  });
 
-    test('should request a player match history', () => {
-      blizzard.sc2.profile('matches', { id: 2137104, name: 'skt' });
+  describe('#player()', () => {
+    test('should request a player account by ID', () => {
+      blizzard.sc2.player({ id: 2137104 });
 
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
       expect(blizzard.axios.get).toHaveBeenCalledWith(
-        'https://us.api.blizzard.com/sc2/profile/2137104/1/skt/matches',
+        'https://us.api.blizzard.com/sc2/player/2137104',
         expect.objectContaining(args),
       );
     });
