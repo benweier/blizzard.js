75741b541af50eb883f0adce104f078c992157eb
Ben Weier <ben.weier@gmail.com>
2018-12-31 16:32:32 +1000

    Update unit tests for refactored application token methods #75

--- test/blizzard.test.js
+++ test/blizzard.test.js
@@ -1,7 +1,6 @@
 const blizzard = require('./initialize');
 const Account = require('../lib/account');
 const Profile = require('../lib/profile');
-const Data = require('../lib/data');
 const Diablo3 = require('../lib/d3');
 const Starcraft2 = require('../lib/sc2');
 const WorldOfWarcraft = require('../lib/wow');
@@ -16,15 +15,14 @@ describe('lib/blizzard.js', () => {
       expect.objectContaining({
         account: expect.any(Account),
         profile: expect.any(Profile),
-        data: expect.any(Data),
         d3: expect.any(Diablo3),
         sc2: expect.any(Starcraft2),
         wow: expect.any(WorldOfWarcraft),
         battletag: expect.any(Function),
         get: expect.any(Function),
         all: expect.any(Function),
-        fetchToken: expect.any(Function),
-        checkToken: expect.any(Function),
+        getApplicationToken: expect.any(Function),
+        validateApplicationToken: expect.any(Function),
       }),
     );
   });
@@ -106,44 +104,73 @@ describe('lib/blizzard.js', () => {
     });
   });
 
-  describe('#fetchToken()', () => {
-    test('should be called with the correct parameters', () => {
-      blizzard.fetchToken({ origin: 'us', type: 'credentials' });
+  describe('#getApplicationToken()', () => {
+    test('should request an oauth token with default parameters', () => {
+      blizzard.getApplicationToken();
 
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
-      expect(blizzard.axios.get).toHaveBeenCalledWith('https://us.battle.net/oauth/token', {
-        auth: expect.objectContaining({
-          username: expect.stringMatching(blizzard.defaults.key),
-          password: expect.stringMatching(blizzard.defaults.secret),
+      expect(blizzard.axios.get).toHaveBeenCalledWith(
+        'https://us.battle.net/oauth/token',
+        expect.objectContaining({
+          auth: {
+            username: blizzard.defaults.key,
+            password: blizzard.defaults.secret,
+          },
+          params: {
+            grant_type: 'client_credentials',
+          },
         }),
-        params: expect.objectContaining({
-          grant_type: 'credentials',
+      );
+    });
+
+    test('should request an oauth token with overridden parameters', () => {
+      blizzard.getApplicationToken({ key: 'yek', secret: 'terces' });
+
+      expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
+      expect(blizzard.axios.get).toHaveBeenCalledWith(
+        'https://us.battle.net/oauth/token',
+        expect.objectContaining({
+          auth: {
+            username: 'yek',
+            password: 'terces',
+          },
+          params: {
+            grant_type: 'client_credentials',
+          },
         }),
-      });
+      );
     });
   });
 
-  describe('#checkToken()', () => {
-    test('should be called with default parameters', () => {
-      blizzard.checkToken();
+  describe('#validateApplicationToken()', () => {
+    test('should validate an oauth token with default parameters', () => {
+      blizzard.validateApplicationToken();
 
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
-      expect(blizzard.axios.get).toHaveBeenCalledWith('https://us.battle.net/oauth/check_token', {
-        params: expect.objectContaining({
-          token: blizzard.defaults.token,
+      expect(blizzard.axios.get).toHaveBeenCalledWith(
+        'https://us.battle.net/oauth/check_token',
+        expect.objectContaining({
+          headers: {
+            'User-Agent': expect.stringMatching(/Node.js\/[0-9].[0-9].[0-9] Blizzard\.js\/[0-9].[0-9].[0-9]/),
+            Authorization: 'Bearer token',
+          },
         }),
-      });
+      );
     });
 
-    test('should be called with overridden parameters', () => {
-      blizzard.checkToken({ origin: 'eu', token: 'test' });
+    test('should validate an oauth token with overridden parameters', () => {
+      blizzard.validateApplicationToken({ token: 'nekot', origin: 'eu' });
 
       expect(blizzard.axios.get).toHaveBeenCalledTimes(1);
-      expect(blizzard.axios.get).toHaveBeenCalledWith('https://eu.battle.net/oauth/check_token', {
-        params: expect.objectContaining({
-          token: 'test',
+      expect(blizzard.axios.get).toHaveBeenCalledWith(
+        'https://eu.battle.net/oauth/check_token',
+        expect.objectContaining({
+          headers: {
+            'User-Agent': expect.stringMatching(/Node.js\/[0-9].[0-9].[0-9] Blizzard\.js\/[0-9].[0-9].[0-9]/),
+            Authorization: 'Bearer nekot',
+          },
         }),
-      });
+      );
     });
   });
 });
