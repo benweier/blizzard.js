d66c1533c25e10df70a87fd800b3a1fedb1db9af
Ben Weier <ben.weier@gmail.com>
2019-01-03 09:51:20 +1000

    Update axios mock for changes to request usage #75

--- test/__mocks__/axios.js
+++ test/__mocks__/axios.js
@@ -1,13 +1,9 @@
-const axios = {
-  create: jest.fn(() => ({
-    get: jest.fn(url => new Promise(_ => _(url))),
-    all: jest.fn(),
-    defaults: {
-      headers: {
-        common: {},
-      },
-    },
-  })),
-};
+const axios = jest.fn(obj => new Promise(_ => _(obj)));
 
-module.exports = axios;
+axios.get = jest.fn(url => new Promise(_ => _(url)));
+axios.post = jest.fn(url => new Promise(_ => _(url)));
+axios.all = jest.fn();
+
+const create = jest.fn(() => axios);
+
+module.exports = { create };
