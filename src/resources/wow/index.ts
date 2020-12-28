10b6d3dba0b761b1596f73635687569ced165d91
Ben Weier <ben.weier@gmail.com>
2020-12-28 17:16:44 +1000

    feat(wow): add resources and client methods for wow profile requests

---
+++ src/resources/wow/index.ts
@@ -0,0 +1,451 @@
+import { Resource } from '..'
+
+/*
+ * PROFILE DATA
+ */
+
+type CharacterOptions = { realm: string; name: string }
+
+export type AccountProfileOptions = Record<string, never>
+
+export const accountProfile = (): Resource => {
+  return {
+    path: 'profile/user/wow',
+    namespace: 'profile',
+  }
+}
+
+export type AccountCharacterProfileOptions = { realmId: number; characterId: number }
+
+export const accountCharacterProfile = ({ realmId, characterId }: AccountCharacterProfileOptions): Resource => {
+  return {
+    path: `/profile/user/wow/protected-character/${realmId}-${characterId}`,
+    namespace: 'profile',
+  }
+}
+
+export type AccountCollectionsOptions = { resource?: 'mounts' | 'pets' }
+
+export const accountCollections = ({ resource }: AccountCollectionsOptions = {}): Resource => {
+  return {
+    path: resource ? `profile/user/wow/collections${resource}` : 'profile/user/wow/collections',
+    namespace: 'profile',
+  }
+}
+
+export type AccountCharacterAchievementsOptions = CharacterOptions & { stats?: boolean }
+
+export const characterAchievements = ({
+  realm,
+  name,
+  stats = false,
+}: AccountCharacterAchievementsOptions): Resource => {
+  return {
+    path: stats
+      ? `profile/wow/character/${realm}/${name}/achievements/statistics`
+      : `profile/wow/character/${realm}/${name}/achievements`,
+    namespace: 'profile',
+  }
+}
+
+export type CharacterAppearanceOptions = CharacterOptions
+
+export const characterAppearance = ({ realm, name }: CharacterAppearanceOptions): Resource => {
+  return {
+    path: `profile/wow/character/${realm}/${name}/appearance`,
+    namespace: 'profile',
+  }
+}
+
+export type CharacterCollectionsOptions = CharacterOptions & { resource?: 'mounts' | 'pets' }
+
+export const characterCollections = ({ realm, name, resource }: CharacterCollectionsOptions): Resource => {
+  return {
+    path:
+      resource !== undefined
+        ? `profile/wow/character/${realm}/${name}/collections/${resource}`
+        : `profile/wow/character/${realm}/${name}/collections`,
+    namespace: 'profile',
+  }
+}
+
+export type CharacterEncountersOptions = CharacterOptions & { resource?: 'dungeons' | 'raids' }
+
+export const characterEncounters = ({ realm, name, resource }: CharacterEncountersOptions): Resource => {
+  return {
+    path:
+      resource !== undefined
+        ? `profile/wow/character/${realm}/${name}/encounters/${resource}`
+        : `profile/wow/character/${realm}/${name}/encounters`,
+    namespace: 'profile',
+  }
+}
+
+export type CharacterEquipmentOptions = CharacterOptions
+
+export const characterEquipment = ({ realm, name }: CharacterEquipmentOptions): Resource => {
+  return {
+    path: `profile/wow/character/${realm}/${name}/equipment`,
+    namespace: 'profile',
+  }
+}
+
+export type CharacterHunterPetsOptions = CharacterOptions
+
+export const characterHunterPets = ({ realm, name }: CharacterHunterPetsOptions): Resource => {
+  return {
+    path: `profile/wow/character/${realm}/${name}/hunter-pets`,
+    namespace: 'profile',
+  }
+}
+
+export type CharacterMediaOptions = CharacterOptions
+
+export const characterMedia = ({ realm, name }: CharacterMediaOptions): Resource => {
+  return {
+    path: `profile/wow/character/${realm}/${name}/character-media`,
+    namespace: 'profile',
+  }
+}
+
+export type CharacterMythicKeystoneOptions = CharacterOptions & { seasonId?: number }
+
+export const characterMythicKeystone = ({ realm, name, seasonId }: CharacterMythicKeystoneOptions): Resource => {
+  return {
+    path:
+      seasonId !== undefined
+        ? `profile/wow/character/${realm}/${name}/mythic-keystone-profile/season/${seasonId}`
+        : `profile/wow/character/${realm}/${name}/mythic-keystone-profile`,
+    namespace: 'profile',
+  }
+}
+
+export type CharacterProfessionsOptions = CharacterOptions
+
+export const characterProfessions = ({ realm, name }: CharacterProfessionsOptions): Resource => {
+  return {
+    path: `profile/wow/character/${realm}/${name}/professions`,
+    namespace: 'profile',
+  }
+}
+
+export type CharacterProfileOptions = CharacterOptions & { status?: boolean }
+
+export const characterProfile = ({ realm, name, status = false }: CharacterProfileOptions): Resource => {
+  return {
+    path: status ? `profile/wow/character/${realm}/${name}/status` : `profile/wow/character/${realm}/${name}`,
+    namespace: 'profile',
+  }
+}
+
+export type CharacterPVPOptions = CharacterOptions & { bracket?: string }
+
+export const characterPVP = ({ realm, name, bracket }: CharacterPVPOptions): Resource => {
+  return {
+    path:
+      bracket !== undefined
+        ? `profile/wow/character/${realm}/${name}/pvp-bracket/${bracket}`
+        : `profile/wow/character/${realm}/${name}/pvp-summary`,
+    namespace: 'profile',
+  }
+}
+
+export type CharacterQuestsOptions = CharacterOptions & { completed?: boolean }
+
+export const characterQuests = ({ realm, name, completed }: CharacterQuestsOptions): Resource => {
+  return {
+    path: completed
+      ? `profile/wow/character/${realm}/${name}/quests/completed`
+      : `profile/wow/character/${realm}/${name}/quests`,
+    namespace: 'profile',
+  }
+}
+
+export type CharacterReputationsOptions = CharacterOptions
+
+export const characterReputations = ({ realm, name }: CharacterReputationsOptions): Resource => {
+  return {
+    path: `profile/wow/character/${realm}/${name}/reputations`,
+    namespace: 'profile',
+  }
+}
+
+export type CharacterSoulbindsOptions = CharacterOptions
+
+export const characterSoulbinds = ({ realm, name }: CharacterSoulbindsOptions): Resource => {
+  return {
+    path: `profile/wow/character/${realm}/${name}/soulbinds`,
+    namespace: 'profile',
+  }
+}
+
+export type CharacterSpecializationsOptions = CharacterOptions
+
+export const characterSpecializations = ({ realm, name }: CharacterSpecializationsOptions): Resource => {
+  return {
+    path: `profile/wow/character/${realm}/${name}/specializations`,
+    namespace: 'profile',
+  }
+}
+
+export type CharacterStatisticsOptions = CharacterOptions
+
+export const characterStatistics = ({ realm, name }: CharacterStatisticsOptions): Resource => {
+  return {
+    path: `profile/wow/character/${realm}/${name}/statistics`,
+    namespace: 'profile',
+  }
+}
+
+export type CharacterTitlesOptions = CharacterOptions
+
+export const characterTitles = ({ realm, name }: CharacterTitlesOptions): Resource => {
+  return {
+    path: `profile/wow/character/${realm}/${name}/titles`,
+    namespace: 'profile',
+  }
+}
+
+export type GuildOptions = { realm: string; name: string; resource?: 'activity' | 'achievements' | 'roster' }
+
+export const guild = ({ realm, name, resource }: GuildOptions): Resource => {
+  return {
+    path: resource !== undefined ? `data/wow/guild/${realm}/${name}/${resource}` : `data/wow/guild/${realm}/${name}`,
+    namespace: 'profile',
+  }
+}
+
+/*
+ * GAME DATA
+ */
+
+// export const achievment = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const auctionHouse = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'dynamic',
+//   }
+// }
+
+// export const azeriteEssence = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const connectedRealm = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'dynamic',
+//   }
+// }
+
+// export const convenant = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const creature = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const guildCrest = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const item = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const journal = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const mediaSearch = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const modifiedCrafting = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const mount = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const mythicKeystoneAffix = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const mythicKeystoneDungeon = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'dynamic',
+//   }
+// }
+
+// export const mythicKeystoneLeaderboard = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'dynamic',
+//   }
+// }
+
+// export const mythicRaidLeaderboard = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'dynamic',
+//   }
+// }
+
+// export const pet = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const playableClass = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const playableRace = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const playableSpecialization = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const powerType = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const profession = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const pvpSeason = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'dynamic',
+//   }
+// }
+
+// export const pvpTier = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const quest = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const realm = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'dynamic',
+//   }
+// }
+
+// export const region = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'dynamic',
+//   }
+// }
+
+// export const reputation = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const spell = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const talent = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const techTalent = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const title = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'static',
+//   }
+// }
+
+// export const token = (): Resource => {
+//   return {
+//     path: '',
+//     namespace: 'dynamic',
+//   }
+// }
--- src/wow/client.ts
+++ src/wow/client.ts
@@ -1,3 +1,119 @@
 import { Blizzard } from '../core'
+import { ResourceOptions, ResourceResponse } from '../resources'
+import * as wow from '../resources/wow'
 
-export class WoW extends Blizzard {}
+export class WoW extends Blizzard {
+  public accountProfile(args?: ResourceOptions<wow.AccountProfileOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.accountProfile)(args)
+
+    return this.get(url, config)
+  }
+
+  public accountCharacterProfile(args: ResourceOptions<wow.AccountCharacterProfileOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.accountCharacterProfile)(args)
+
+    return this.get(url, config)
+  }
+
+  public accountCollections(args: ResourceOptions<wow.AccountCollectionsOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.accountCollections)(args)
+
+    return this.get(url, config)
+  }
+
+  public characterAchievements(args: ResourceOptions<wow.AccountCharacterAchievementsOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.characterAchievements)(args)
+
+    return this.get(url, config)
+  }
+
+  public characterAppearance(args: ResourceOptions<wow.CharacterAppearanceOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.characterAppearance)(args)
+
+    return this.get(url, config)
+  }
+
+  public characterEncounters(args: ResourceOptions<wow.CharacterEncountersOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.characterEncounters)(args)
+
+    return this.get(url, config)
+  }
+
+  public characterEquipment(args: ResourceOptions<wow.CharacterEquipmentOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.characterEquipment)(args)
+
+    return this.get(url, config)
+  }
+
+  public characterHunterPets(args: ResourceOptions<wow.CharacterHunterPetsOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.characterHunterPets)(args)
+
+    return this.get(url, config)
+  }
+
+  public characterMythicKeystone(args: ResourceOptions<wow.CharacterMythicKeystoneOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.characterMythicKeystone)(args)
+
+    return this.get(url, config)
+  }
+
+  public characterProfessions(args: ResourceOptions<wow.CharacterProfessionsOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.characterProfessions)(args)
+
+    return this.get(url, config)
+  }
+
+  public characterProfile(args: ResourceOptions<wow.CharacterProfileOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.characterProfile)(args)
+
+    return this.get(url, config)
+  }
+
+  public characterPVP(args: ResourceOptions<wow.CharacterPVPOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.characterPVP)(args)
+
+    return this.get(url, config)
+  }
+
+  public characterQuests(args: ResourceOptions<wow.CharacterQuestsOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.characterQuests)(args)
+
+    return this.get(url, config)
+  }
+
+  public characterReputations(args: ResourceOptions<wow.CharacterReputationsOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.characterReputations)(args)
+
+    return this.get(url, config)
+  }
+
+  public characterSoulbinds(args: ResourceOptions<wow.CharacterSoulbindsOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.characterSoulbinds)(args)
+
+    return this.get(url, config)
+  }
+
+  public characterSpecializations(args: ResourceOptions<wow.CharacterSpecializationsOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.characterSpecializations)(args)
+
+    return this.get(url, config)
+  }
+
+  public characterStatistics(args: ResourceOptions<wow.CharacterStatisticsOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.characterStatistics)(args)
+
+    return this.get(url, config)
+  }
+
+  public characterTitles(args: ResourceOptions<wow.CharacterTitlesOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.characterTitles)(args)
+
+    return this.get(url, config)
+  }
+
+  public guild(args: ResourceOptions<wow.GuildOptions>): ResourceResponse {
+    const [url, config] = this.createClientResourceRequest(wow.guild)(args)
+
+    return this.get(url, config)
+  }
+}
