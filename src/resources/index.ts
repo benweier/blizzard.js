31aadf30203c9190630e4b1f500353809fd9d736
Ben Weier <ben.weier@gmail.com>
2020-12-29 23:22:09 +1000

    feat(ts): add protected resource interface for profile namespace

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,11 +1,15 @@
 import { AxiosResponse } from 'axios'
-import { ClientOptions } from '../core'
+import { AccessToken, ClientOptions } from '../core'
 export * as wow from './wow'
 
 export type ResourceResponse<T = any> = Promise<AxiosResponse<T>>
 
 export interface ResourceInterface<T = any> {
   (args: ResourceOptions<T>): Resource
+}
+
+export interface ProtectedResourceInterface<T = any> {
+  (args: ProtectedResourceOptions<T>): Resource
 }
 
 export type Resource<T = never> = {
@@ -15,3 +19,4 @@ export type Resource<T = never> = {
 }
 
 export type ResourceOptions<T> = Partial<ClientOptions> & T
+export type ProtectedResourceOptions<T> = Partial<ClientOptions> & { token: AccessToken } & T
--- src/wow/client.ts
+++ src/wow/client.ts
@@ -1,117 +1,121 @@
 import { Blizzard } from '../core'
-import { ResourceOptions, ResourceResponse } from '../resources'
+import { ProtectedResourceOptions, ResourceResponse } from '../resources'
 import * as wow from '../resources/wow'
 
 export class WoW extends Blizzard {
-  public accountProfile(args?: ResourceOptions<wow.AccountProfileOptions>): ResourceResponse {
+  public accountProfile(args?: ProtectedResourceOptions<wow.AccountProfileOptions>): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.accountProfile)(args)
 
     return this.get(url, config)
   }
 
-  public accountCharacterProfile(args: ResourceOptions<wow.AccountCharacterProfileOptions>): ResourceResponse {
+  public accountCharacterProfile(args: ProtectedResourceOptions<wow.AccountCharacterProfileOptions>): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.accountCharacterProfile)(args)
 
     return this.get(url, config)
   }
 
-  public accountCollections(args: ResourceOptions<wow.AccountCollectionsOptions>): ResourceResponse {
+  public accountCollections(args: ProtectedResourceOptions<wow.AccountCollectionsOptions>): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.accountCollections)(args)
 
     return this.get(url, config)
   }
 
-  public characterAchievements(args: ResourceOptions<wow.AccountCharacterAchievementsOptions>): ResourceResponse {
+  public characterAchievements(
+    args: ProtectedResourceOptions<wow.AccountCharacterAchievementsOptions>,
+  ): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.characterAchievements)(args)
 
     return this.get(url, config)
   }
 
-  public characterAppearance(args: ResourceOptions<wow.CharacterAppearanceOptions>): ResourceResponse {
+  public characterAppearance(args: ProtectedResourceOptions<wow.CharacterAppearanceOptions>): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.characterAppearance)(args)
 
     return this.get(url, config)
   }
 
-  public characterEncounters(args: ResourceOptions<wow.CharacterEncountersOptions>): ResourceResponse {
+  public characterEncounters(args: ProtectedResourceOptions<wow.CharacterEncountersOptions>): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.characterEncounters)(args)
 
     return this.get(url, config)
   }
 
-  public characterEquipment(args: ResourceOptions<wow.CharacterEquipmentOptions>): ResourceResponse {
+  public characterEquipment(args: ProtectedResourceOptions<wow.CharacterEquipmentOptions>): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.characterEquipment)(args)
 
     return this.get(url, config)
   }
 
-  public characterHunterPets(args: ResourceOptions<wow.CharacterHunterPetsOptions>): ResourceResponse {
+  public characterHunterPets(args: ProtectedResourceOptions<wow.CharacterHunterPetsOptions>): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.characterHunterPets)(args)
 
     return this.get(url, config)
   }
 
-  public characterMythicKeystone(args: ResourceOptions<wow.CharacterMythicKeystoneOptions>): ResourceResponse {
+  public characterMythicKeystone(args: ProtectedResourceOptions<wow.CharacterMythicKeystoneOptions>): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.characterMythicKeystone)(args)
 
     return this.get(url, config)
   }
 
-  public characterProfessions(args: ResourceOptions<wow.CharacterProfessionsOptions>): ResourceResponse {
+  public characterProfessions(args: ProtectedResourceOptions<wow.CharacterProfessionsOptions>): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.characterProfessions)(args)
 
     return this.get(url, config)
   }
 
-  public characterProfile(args: ResourceOptions<wow.CharacterProfileOptions>): ResourceResponse {
+  public characterProfile(args: ProtectedResourceOptions<wow.CharacterProfileOptions>): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.characterProfile)(args)
 
     return this.get(url, config)
   }
 
-  public characterPVP(args: ResourceOptions<wow.CharacterPVPOptions>): ResourceResponse {
+  public characterPVP(args: ProtectedResourceOptions<wow.CharacterPVPOptions>): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.characterPVP)(args)
 
     return this.get(url, config)
   }
 
-  public characterQuests(args: ResourceOptions<wow.CharacterQuestsOptions>): ResourceResponse {
+  public characterQuests(args: ProtectedResourceOptions<wow.CharacterQuestsOptions>): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.characterQuests)(args)
 
     return this.get(url, config)
   }
 
-  public characterReputations(args: ResourceOptions<wow.CharacterReputationsOptions>): ResourceResponse {
+  public characterReputations(args: ProtectedResourceOptions<wow.CharacterReputationsOptions>): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.characterReputations)(args)
 
     return this.get(url, config)
   }
 
-  public characterSoulbinds(args: ResourceOptions<wow.CharacterSoulbindsOptions>): ResourceResponse {
+  public characterSoulbinds(args: ProtectedResourceOptions<wow.CharacterSoulbindsOptions>): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.characterSoulbinds)(args)
 
     return this.get(url, config)
   }
 
-  public characterSpecializations(args: ResourceOptions<wow.CharacterSpecializationsOptions>): ResourceResponse {
+  public characterSpecializations(
+    args: ProtectedResourceOptions<wow.CharacterSpecializationsOptions>,
+  ): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.characterSpecializations)(args)
 
     return this.get(url, config)
   }
 
-  public characterStatistics(args: ResourceOptions<wow.CharacterStatisticsOptions>): ResourceResponse {
+  public characterStatistics(args: ProtectedResourceOptions<wow.CharacterStatisticsOptions>): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.characterStatistics)(args)
 
     return this.get(url, config)
   }
 
-  public characterTitles(args: ResourceOptions<wow.CharacterTitlesOptions>): ResourceResponse {
+  public characterTitles(args: ProtectedResourceOptions<wow.CharacterTitlesOptions>): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.characterTitles)(args)
 
     return this.get(url, config)
   }
 
-  public guild(args: ResourceOptions<wow.GuildOptions>): ResourceResponse {
+  public guild(args: ProtectedResourceOptions<wow.GuildOptions>): ResourceResponse {
     const [url, config] = this.createClientResourceRequest(wow.guild)(args)
 
     return this.get(url, config)
