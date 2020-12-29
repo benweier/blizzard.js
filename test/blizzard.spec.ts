2e3fac8c1a2e89cc1ec462707cf1b934293315de
Ben Weier <ben.weier@gmail.com>
2020-12-29 11:56:48 +1000

    test(core): assert create and prepare methods

--- test/blizzard.spec.ts
+++ test/blizzard.spec.ts
@@ -4,6 +4,8 @@ describe('Blizzard', () => {
   test('should implement all core methods', async () => {
     expect(Blizzard.prototype).toHaveProperty('get', expect.any(Function))
     expect(Blizzard.prototype).toHaveProperty('post', expect.any(Function))
+    expect(Blizzard.prototype).toHaveProperty('createClientResourceRequest', expect.any(Function))
+    expect(Blizzard.prototype).toHaveProperty('prepareResourceRequest', expect.any(Function))
     expect(Blizzard.prototype).toHaveProperty('getApplicationToken', expect.any(Function))
     expect(Blizzard.prototype).toHaveProperty('setApplicationToken', expect.any(Function))
     expect(Blizzard.prototype).toHaveProperty('validateApplicationToken', expect.any(Function))
