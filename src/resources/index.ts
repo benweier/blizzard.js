8c71252c74da719a2115d46654d09876d7d67414
Ben Weier <ben.weier@gmail.com>
2020-12-31 23:29:34 +1000

    chore(ts): update resource typings

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,15 +1,15 @@
 import { AxiosResponse } from 'axios'
-import { AccessToken, ClientOptions } from '../core'
+import { ClientOptions } from '../core'
 export * as wow from './wow'
 
 export type ResourceResponse<T = any> = Promise<AxiosResponse<T>>
 
-export interface ResourceInterface<T = any> {
-  (args: ResourceOptions<T>): Resource
+export interface ResourceInterface<T = any, P = any> {
+  (args: ResourceOptions<T>): Resource<P>
 }
 
-export interface ProtectedResourceInterface<T = any> {
-  (args: ProtectedResourceOptions<T>): Resource
+export interface ProtectedResourceInterface<T = any, P = any> {
+  (args: ProtectedResourceOptions<T>): Resource<P>
 }
 
 export type Resource<T = never> = {
@@ -19,4 +19,4 @@ export type Resource<T = never> = {
 }
 
 export type ResourceOptions<T> = Partial<ClientOptions> & T
-export type ProtectedResourceOptions<T> = Partial<ClientOptions> & { token: AccessToken } & T
+export type ProtectedResourceOptions<T> = Partial<ClientOptions> & { token: string } & T
