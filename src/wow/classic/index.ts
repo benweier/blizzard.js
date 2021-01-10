9e2f01b2707acf20ca6f3d3a6c02f617c713b023
Ben Weier <ben.weier@gmail.com>
2021-01-10 18:41:14 +1000

    fix: remove circular import

--- src/wow/classic/index.ts
+++ src/wow/classic/index.ts
@@ -1,6 +1,5 @@
 import { createClient } from '../../create-client'
 import { WoWClassic, WoWClassicClient } from './client'
-export * as classic from '../classic'
 
 export const createInstance = createClient<WoWClassicClient>(WoWClassic)
 export { WoWClassic }
