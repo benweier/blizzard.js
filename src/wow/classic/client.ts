6a8520a023f26e5fb6517d31cd93edac611d1106
Ben Weier <ben.weier@gmail.com>
2021-01-10 18:37:04 +1000

    feat(wow): add wow classic client and all supported resources

--- src/wow/classic/client.ts
+++ src/wow/classic/client.ts
@@ -1,3 +1,40 @@
-import { Blizzard } from '../../core'
+import { Blizzard, BlizzardClient } from '../../core'
+import { ResourceResponse, ResourceOptions } from '../../resources'
+import * as wow from '../../resources/wow'
 
-export class WoW extends Blizzard {}
+export interface WoWClassicClient extends BlizzardClient {
+  connectedRealm<T = any>(args?: ResourceOptions<wow.ConnectedRealmOptions>): ResourceResponse<T>
+  connectedRealmSearch<T = any>(args: ResourceOptions<wow.ConnectedRealmSearchOptions>): ResourceResponse<T>
+  creature<T = any>(args: ResourceOptions<wow.CreatureOptions>): ResourceResponse<T>
+  guildCrest<T = any>(args?: ResourceOptions<wow.GuildCrestOptions>): ResourceResponse<T>
+  item<T = any>(args: ResourceOptions<wow.ItemOptions>): ResourceResponse<T>
+  itemSearch<T = any>(args: ResourceOptions<wow.ItemSearchOptions>): ResourceResponse<T>
+  mediaSearch<T = any>(args: ResourceOptions<wow.MediaSearchOptions>): ResourceResponse<T>
+  playableClass<T = any>(args?: ResourceOptions<wow.PlayableClassOptions>): ResourceResponse<T>
+  playableRace<T = any>(args?: ResourceOptions<wow.PlayableRaceOptions>): ResourceResponse<T>
+  powerType<T = any>(args?: ResourceOptions<wow.PowerTypeOptions>): ResourceResponse<T>
+  realm<T = any>(args?: ResourceOptions<wow.RealmOptions>): ResourceResponse<T>
+  realmSearch<T = any>(args: ResourceOptions<wow.RealmSearchOptions>): ResourceResponse<T>
+  region<T = any>(args?: ResourceOptions<wow.RegionOptions>): ResourceResponse<T>
+  token<T = any>(args?: ResourceOptions<wow.TokenOptions>): ResourceResponse<T>
+}
+
+export class WoWClassic extends Blizzard implements WoWClassicClient {
+  connectedRealm = this.createClientResourceRequest(wow.connectedRealm)
+  connectedRealmSearch = this.createClientResourceRequest(wow.connectedRealmSearch)
+  creature = this.createClientResourceRequest(wow.creature)
+  creatureFamily = this.createClientResourceRequest(wow.creatureFamily)
+  creatureSearch = this.createClientResourceRequest(wow.creatureSearch)
+  creatureType = this.createClientResourceRequest(wow.creatureType)
+  guildCrest = this.createClientResourceRequest(wow.guildCrest)
+  item = this.createClientResourceRequest(wow.item)
+  itemSearch = this.createClientResourceRequest(wow.itemSearch)
+  mediaSearch = this.createClientResourceRequest(wow.mediaSearch)
+  playableClass = this.createClientResourceRequest(wow.playableClass)
+  playableRace = this.createClientResourceRequest(wow.playableRace)
+  powerType = this.createClientResourceRequest(wow.powerType)
+  realm = this.createClientResourceRequest(wow.realm)
+  realmSearch = this.createClientResourceRequest(wow.realmSearch)
+  region = this.createClientResourceRequest(wow.region)
+  token = this.createClientResourceRequest(wow.token)
+}
--- src/wow/classic/index.ts
+++ src/wow/classic/index.ts
@@ -1,5 +1,7 @@
 import { createClient } from '../../create-client'
-import { WoW } from './client'
+import { WoWClassic, WoWClassicClient } from './client'
+export * as classic from '../classic'
 
-export const createInstance = createClient<WoW>(WoW)
-export { WoW }
+export const createInstance = createClient<WoWClassicClient>(WoWClassic)
+export { WoWClassic }
+export type { WoWClassicClient }
