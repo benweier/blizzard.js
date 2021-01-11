2cc174d29f9dfbdf9d4899b2f1af5925cb33c2ce
Ben Weier <ben.weier@gmail.com>
2021-01-11 23:27:40 +1000

    feat(sc2): add typings for sc2 API resources

--- src/resources/sc2.ts
+++ src/resources/sc2.ts
@@ -7,3 +7,14 @@ export const tmp = (): Resource => {
     path: '',
   }
 }
+
+export type ProfileOptions = { region: number; realm?: number; profile?: number }
+export type MetaDataOptions = { region: number; realm: number; profile: number }
+export type ProfileLadderOptions = { id?: number }
+export type LadderOptions = { region: number; resource: 'grandmaster' | 'season' }
+export type AccountOptions = { id: string }
+export type LegacyProfileOptions = { region: number; realm: number; profile: number; resource?: 'ladders' | 'matches' }
+export type LegacyLadderOptions = { region: number; ladder: number }
+export type LegacyAchievementsOptions = { region: number }
+export type LegacyRewardsOptions = { region: number }
+export type LeagueOptions = { season: number; queue: number; teamType: number; league: number }
--- src/sc2/client.ts
+++ src/sc2/client.ts
@@ -3,7 +3,16 @@ import { ResourceResponse, ResourceOptions } from '../resources'
 import * as sc2 from '../resources/sc2'
 
 export interface SC2Client extends BlizzardClient {
-  tmp<T = any>(args: ResourceOptions<any>): ResourceResponse<T>
+  profile<T = any>(args: ResourceOptions<sc2.ProfileOptions>): ResourceResponse<T>
+  metadata<T = any>(args: ResourceOptions<sc2.MetaDataOptions>): ResourceResponse<T>
+  profileLadder<T = any>(args?: ResourceOptions<sc2.ProfileLadderOptions>): ResourceResponse<T>
+  ladder<T = any>(args: ResourceOptions<sc2.LadderOptions>): ResourceResponse<T>
+  account<T = any>(args: ResourceOptions<sc2.AccountOptions>): ResourceResponse<T>
+  legacyProfile<T = any>(args: ResourceOptions<sc2.LegacyProfileOptions>): ResourceResponse<T>
+  legacyLadder<T = any>(args: ResourceOptions<sc2.LegacyLadderOptions>): ResourceResponse<T>
+  legacyAchievements<T = any>(args: ResourceOptions<sc2.LegacyAchievementsOptions>): ResourceResponse<T>
+  legacyRewards<T = any>(args: ResourceOptions<sc2.LegacyRewardsOptions>): ResourceResponse<T>
+  league<T = any>(args: ResourceOptions<sc2.LeagueOptions>): ResourceResponse<T>
 }
 
 export class SC2 extends Blizzard implements SC2Client {
