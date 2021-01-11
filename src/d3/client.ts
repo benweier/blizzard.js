6516e6c2f12bb161f8397b0baa4be780acb7ae4e
Ben Weier <ben.weier@gmail.com>
2021-01-11 23:28:03 +1000

    feat(d3): add typings for d3 API resources

--- src/d3/client.ts
+++ src/d3/client.ts
@@ -3,7 +3,15 @@ import { ResourceResponse, ResourceOptions } from '../resources'
 import * as d3 from '../resources/d3'
 
 export interface D3Client extends BlizzardClient {
-  tmp<T = any>(args: ResourceOptions<any>): ResourceResponse<T>
+  act<T = any>(args?: ResourceOptions<d3.ActOptions>): ResourceResponse<T>
+  artisan<T = any>(args: ResourceOptions<d3.ArtisanOptions>): ResourceResponse<T>
+  recipe<T = any>(args: ResourceOptions<d3.RecipeOptions>): ResourceResponse<T>
+  follower<T = any>(args: ResourceOptions<d3.FollowerOptions>): ResourceResponse<T>
+  characterClass<T = any>(args: ResourceOptions<d3.CharacterClassOptions>): ResourceResponse<T>
+  characterSkill<T = any>(args: ResourceOptions<d3.CharacterSkillOptions>): ResourceResponse<T>
+  item<T = any>(args: ResourceOptions<d3.ItemOptions>): ResourceResponse<T>
+  itemType<T = any>(args: ResourceOptions<d3.ItemTypeOptions>): ResourceResponse<T>
+  accountProfile<T = any>(args: ResourceOptions<d3.AccountProfile>): ResourceResponse<T>
 }
 
 export class D3 extends Blizzard implements D3Client {
--- src/resources/d3.ts
+++ src/resources/d3.ts
@@ -7,3 +7,16 @@ export const tmp = (): Resource => {
     path: '',
   }
 }
+
+export type ActOptions = { id?: number }
+export type ArtisanOptions = { id: string }
+export type RecipeOptions = { artisan: string; recipe: string }
+export type FollowerOptions = { id: string }
+export type CharacterClassOptions = { id: string }
+export type CharacterSkillOptions = { class: string; skill: string }
+export type ItemOptions = { id: string }
+export type ItemTypeOptions = { id?: string }
+export type AccountProfile = { account: string; hero?: string; resource?: 'items' | 'follower-items' }
+
+export type SeasonOptions = { id?: number; leadreboard?: number }
+export type EraOptions = { id?: number; leaderboard?: number }
