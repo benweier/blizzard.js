6b4ba994d0cc19da866bec0e520655a936e083a1
Ben Weier <ben.weier@gmail.com>
2021-01-12 22:27:53 +1000

    feat(d3): add d3 client resources

--- src/d3/client.ts
+++ src/d3/client.ts
@@ -10,10 +10,22 @@ export interface D3Client extends BlizzardClient {
   characterClass<T = any>(args: ResourceOptions<d3.CharacterClassOptions>): ResourceResponse<T>
   characterSkill<T = any>(args: ResourceOptions<d3.CharacterSkillOptions>): ResourceResponse<T>
   item<T = any>(args: ResourceOptions<d3.ItemOptions>): ResourceResponse<T>
-  itemType<T = any>(args: ResourceOptions<d3.ItemTypeOptions>): ResourceResponse<T>
+  itemType<T = any>(args?: ResourceOptions<d3.ItemTypeOptions>): ResourceResponse<T>
   accountProfile<T = any>(args: ResourceOptions<d3.AccountProfile>): ResourceResponse<T>
+  season<T = any>(args?: ResourceOptions<d3.SeasonOptions>): ResourceResponse<T>
+  era<T = any>(args?: ResourceOptions<d3.EraOptions>): ResourceResponse<T>
 }
 
 export class D3 extends Blizzard implements D3Client {
-  tmp = this.createClientResourceRequest(d3.tmp)
+  act = this.createClientResourceRequest(d3.act)
+  artisan = this.createClientResourceRequest(d3.artisan)
+  recipe = this.createClientResourceRequest(d3.recipe)
+  follower = this.createClientResourceRequest(d3.follower)
+  characterClass = this.createClientResourceRequest(d3.characterClass)
+  characterSkill = this.createClientResourceRequest(d3.characterSkill)
+  item = this.createClientResourceRequest(d3.item)
+  itemType = this.createClientResourceRequest(d3.itemType)
+  accountProfile = this.createClientResourceRequest(d3.accountProfile)
+  season = this.createClientResourceRequest(d3.season)
+  era = this.createClientResourceRequest(d3.era)
 }
--- src/resources/d3.ts
+++ src/resources/d3.ts
@@ -1,22 +1,116 @@
 import { Resource } from '.'
 
-export type TmpOptions = Record<string, unknown>
+export type ActOptions = { id?: number }
 
-export const tmp = (): Resource => {
+export const act = (args?: ActOptions): Resource => {
   return {
-    path: '',
+    path: args?.id === undefined ? `d3/data/act` : `d3/data/act/${args.id}`,
   }
 }
 
-export type ActOptions = { id?: number }
 export type ArtisanOptions = { id: string }
+
+export const artisan = (args: ArtisanOptions): Resource => {
+  return {
+    path: `d3/data/artisan/${args.id}`,
+  }
+}
+
 export type RecipeOptions = { artisan: string; recipe: string }
+
+export const recipe = (args: RecipeOptions): Resource => {
+  return {
+    path: `d3/data/artisan/${args.artisan}/recipe/${args.recipe}`,
+  }
+}
+
 export type FollowerOptions = { id: string }
+
+export const follower = (args: FollowerOptions): Resource => {
+  return {
+    path: `d3/data/follower/${args.id}`,
+  }
+}
+
 export type CharacterClassOptions = { id: string }
+
+export const characterClass = (args: CharacterClassOptions): Resource => {
+  return {
+    path: `d3/data/hero/${args.id}`,
+  }
+}
+
 export type CharacterSkillOptions = { class: string; skill: string }
+
+export const characterSkill = (args: CharacterSkillOptions): Resource => {
+  return {
+    path: `d3/data/hero/${args.class}/skill/${args.skill}`,
+  }
+}
+
 export type ItemOptions = { id: string }
+
+export const item = (args: ItemOptions): Resource => {
+  return {
+    path: `d3/data/item/${args.id}`,
+  }
+}
+
 export type ItemTypeOptions = { id?: string }
+
+export const itemType = (args?: ItemTypeOptions): Resource => {
+  return {
+    path: args?.id === undefined ? 'd3/data/item-type' : `d3/data/item-type/${args.id}`,
+  }
+}
+
 export type AccountProfile = { account: string; hero?: string; resource?: 'items' | 'follower-items' }
 
-export type SeasonOptions = { id?: number; leadreboard?: number }
+export const accountProfile = (args: AccountProfile): Resource => {
+  if (args.hero === undefined) {
+    return {
+      path: `d3/profile/${args.account}/`,
+    }
+  }
+
+  return {
+    path:
+      args.resource === undefined
+        ? `d3/profile/${args.account}/hero/${args.hero}`
+        : `d3/profile/${args.account}/hero/${args.hero}/${args.resource}`,
+  }
+}
+
+export type SeasonOptions = { id?: number; leaderboard?: number }
+
+export const season = (args?: SeasonOptions): Resource => {
+  if (args?.id === undefined) {
+    return {
+      path: 'data/d3/season/',
+    }
+  }
+
+  return {
+    path:
+      args.leaderboard === undefined
+        ? `data/d3/season/${args.id}`
+        : `data/d3/season/${args.id}/leaderboard/${args.leaderboard}`,
+  }
+}
+
 export type EraOptions = { id?: number; leaderboard?: number }
+
+export const era = (args?: EraOptions): Resource => {
+  if (args?.id === undefined) {
+    return {
+      path: 'data/d3/era/',
+    }
+  }
+
+  return {
+    path:
+      args.leaderboard === undefined
+        ? `data/d3/era/${args.id}`
+        : `data/d3/era/${args.id}/leaderboard/${args.leaderboard}`,
+  }
+}
