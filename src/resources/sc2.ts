f230319989497de27ad3d1e0776b9eaffae43763
Ben Weier <ben.weier@gmail.com>
2021-01-12 23:16:03 +1000

    feat(sc2): add sc2 client resources

--- src/resources/sc2.ts
+++ src/resources/sc2.ts
@@ -1,20 +1,95 @@
 import { Resource } from '.'
 
-export type TmpOptions = Record<string, unknown>
+export type ProfileOptions = { region: 1 | 2 | 3; realm?: 1 | 2; profile?: number }
 
-export const tmp = (): Resource => {
+export const profile = (args: ProfileOptions): Resource => {
   return {
-    path: '',
+    path:
+      args.realm === undefined || args.profile === undefined
+        ? `sc2/static/profile/${args.region}`
+        : `sc2/static/profile/${args.region}`,
   }
 }
 
-export type ProfileOptions = { region: number; realm?: number; profile?: number }
-export type MetaDataOptions = { region: number; realm: number; profile: number }
-export type ProfileLadderOptions = { id?: number }
-export type LadderOptions = { region: number; resource: 'grandmaster' | 'season' }
+export type MetaDataOptions = { region: 1 | 2 | 3; realm: 1 | 2; profile: number }
+
+export const metadata = (args: MetaDataOptions): Resource => {
+  return {
+    path: `sc2/metadata/profile/${args.region}/${args.realm}/${args.profile}`,
+  }
+}
+
+export type ProfileLadderOptions = { region: 1 | 2 | 3; realm: 1 | 2; profile: number; id?: number }
+
+export const profileLadder = (args: ProfileLadderOptions): Resource => {
+  return {
+    path:
+      args.id === undefined
+        ? `sc2/profile/${args.region}/${args.realm}/${args.profile}/ladder/summary`
+        : `sc2/profile/${args.region}/${args.realm}/${args.profile}/ladder/${args.id}`,
+  }
+}
+
+export type LadderOptions = { region: 1 | 2 | 3; resource: 'grandmaster' | 'season' }
+
+export const ladder = (args: LadderOptions): Resource => {
+  return {
+    path: `sc2/ladder/${args.resource}/${args.region}`,
+  }
+}
+
 export type AccountOptions = { id: string }
-export type LegacyProfileOptions = { region: number; realm: number; profile: number; resource?: 'ladders' | 'matches' }
-export type LegacyLadderOptions = { region: number; ladder: number }
-export type LegacyAchievementsOptions = { region: number }
-export type LegacyRewardsOptions = { region: number }
-export type LeagueOptions = { season: number; queue: number; teamType: number; league: number }
+
+export const account = (args: AccountOptions): Resource => {
+  return {
+    path: `sc2/player/${args.id}`,
+  }
+}
+
+export type LegacyProfileOptions = {
+  region: 1 | 2 | 3
+  realm: 1 | 2
+  profile: number
+  resource?: 'ladders' | 'matches'
+}
+
+export const legacyProfile = (args: LegacyProfileOptions): Resource => {
+  return {
+    path:
+      args.resource === undefined
+        ? `sc2/legacy/profile/${args.region}/${args.realm}/${args.profile}`
+        : `sc2/legacy/profile/${args.region}/${args.realm}/${args.profile}/${args.resource}`,
+  }
+}
+
+export type LegacyLadderOptions = { region: 1 | 2 | 3; ladder: number }
+
+export const legacyLadder = (args: LegacyLadderOptions): Resource => {
+  return {
+    path: `sc2/legacy/ladder/${args.region}/${args.ladder}`,
+  }
+}
+
+export type LegacyAchievementsOptions = { region: 1 | 2 | 3 }
+
+export const legacyAchievements = (args: LegacyAchievementsOptions): Resource => {
+  return {
+    path: `sc2/legacy/achievements/${args.region}`,
+  }
+}
+
+export type LegacyRewardsOptions = { region: 1 | 2 | 3 }
+
+export const legacyRewards = (args: LegacyRewardsOptions): Resource => {
+  return {
+    path: `sc2/legacy/rewards/${args.region}`,
+  }
+}
+
+export type LeagueOptions = { season: number; queue: number; teamType: 0 | 1; league: 0 | 1 | 2 | 3 | 4 | 5 | 6 }
+
+export const league = (args: LeagueOptions): Resource => {
+  return {
+    path: `data/sc2/league/${args.season}/${args.queue}/${args.teamType}/${args.league}`,
+  }
+}
--- src/sc2/client.ts
+++ src/sc2/client.ts
@@ -5,7 +5,7 @@ import * as sc2 from '../resources/sc2'
 export interface SC2Client extends BlizzardClient {
   profile<T = any>(args: ResourceOptions<sc2.ProfileOptions>): ResourceResponse<T>
   metadata<T = any>(args: ResourceOptions<sc2.MetaDataOptions>): ResourceResponse<T>
-  profileLadder<T = any>(args?: ResourceOptions<sc2.ProfileLadderOptions>): ResourceResponse<T>
+  profileLadder<T = any>(args: ResourceOptions<sc2.ProfileLadderOptions>): ResourceResponse<T>
   ladder<T = any>(args: ResourceOptions<sc2.LadderOptions>): ResourceResponse<T>
   account<T = any>(args: ResourceOptions<sc2.AccountOptions>): ResourceResponse<T>
   legacyProfile<T = any>(args: ResourceOptions<sc2.LegacyProfileOptions>): ResourceResponse<T>
@@ -16,5 +16,14 @@ export interface SC2Client extends BlizzardClient {
 }
 
 export class SC2 extends Blizzard implements SC2Client {
-  tmp = this.createClientResourceRequest(sc2.tmp)
+  profile = this.createClientResourceRequest(sc2.profile)
+  metadata = this.createClientResourceRequest(sc2.metadata)
+  profileLadder = this.createClientResourceRequest(sc2.profileLadder)
+  ladder = this.createClientResourceRequest(sc2.ladder)
+  account = this.createClientResourceRequest(sc2.account)
+  legacyProfile = this.createClientResourceRequest(sc2.legacyProfile)
+  legacyLadder = this.createClientResourceRequest(sc2.legacyLadder)
+  legacyAchievements = this.createClientResourceRequest(sc2.legacyAchievements)
+  legacyRewards = this.createClientResourceRequest(sc2.legacyRewards)
+  league = this.createClientResourceRequest(sc2.league)
 }
