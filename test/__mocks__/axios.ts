77d700f3fd62e22613a670349c41e36732c93b29
Ben Weier <ben.weier@gmail.com>
2021-01-16 18:35:51 +1000

    test: add conditions for expired and expiring tokens

--- test/__mocks__/axios.ts
+++ test/__mocks__/axios.ts
@@ -12,15 +12,46 @@ mockAxios.onGet('https://us.battle.net/oauth/token').reply(200, {
   expires_in: 86400, // 1 day in seconds
 })
 
-mockAxios.onPost('https://us.battle.net/oauth/check_token').reply(200, {
-  scope: [],
-  exp: (Date.now() + 43200000) / 1000, // +12hrs in seconds
-  authorities: [
+mockAxios.onPost('https://us.battle.net/oauth/check_token').reply((config) => {
+  if (config.data.includes('token=expired')) {
+    return [
+      400,
+      {
+        error: 'invalid_token',
+        error_description: 'Token was not recognised',
+      },
+    ]
+  }
+
+  if (config.data.includes('token=expiring')) {
+    return [
+      200,
+      {
+        scope: [],
+        exp: (Date.now() + 30000) / 1000, // +30 seconds
+        authorities: [
+          {
+            authority: 'test_authority',
+          },
+        ],
+        client_id: 'test_id',
+      },
+    ]
+  }
+
+  return [
+    200,
     {
-      authority: 'test_authority',
+      scope: [],
+      exp: (Date.now() + 43200000) / 1000, // +12hrs in seconds
+      authorities: [
+        {
+          authority: 'test_authority',
+        },
+      ],
+      client_id: 'test_id',
     },
-  ],
-  client_id: 'test_id',
+  ]
 })
 
 mockAxios.onAny().reply(200)
