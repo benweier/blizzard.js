bb3a9f5d375fc4bbfdedc3908a4c425ec7c45490
Ben Weier <ben.weier@gmail.com>
2022-08-29 20:54:55 +1000

    chore(test): add globals and roots config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.json',
+    },
+  },
   testEnvironment: 'node',
+  roots: ['<rootDir>/test'],
 }
